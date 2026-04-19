8843d903f61a3ebff8cb053f7081e0306a04db37
Pratheek Gowda <pratheek@myyahoo.com>
2026-04-19 21:11:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // This tells Vercel to skip the "grammar" check
-    ignoreDuringBuilds: true,
+{
+  "name": "pratheek-enterprises-portal",
+  "version": "0.1.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
   },
-  typescript: {
-    // This tells Vercel to skip the strict type check
-    ignoreBuildErrors: true,
+  "dependencies": {
+    "@clerk/nextjs": "5.7.2",
+    "next": "14.2.23",
+    "react": "18.3.1",
+    "react-dom": "18.3.1"
   },
-};
-
-module.exports = nextConfig;
+  "devDependencies": {
+    "@types/node": "^20",
+    "@types/react": "18.3.1",
+    "@types/react-dom": "18.3.1",
+    "typescript": "^5"
+  }
+}
